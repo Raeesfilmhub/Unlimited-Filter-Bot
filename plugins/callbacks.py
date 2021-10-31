eb785f05ee066cdf91de1ad91fbb31eba0640c8b
Raeesfilmhub <87624395+Raeesfilmhub@users.noreply.github.com>
2021-11-01 01:11:06 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,11 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("🔰Join Our Group 🔰", url="https://youtu.be/hkmc3e7U7R4"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("💎Support Group💎", url="https://t.me/efxado")
                 ]
             ]
         )
@@ -70,7 +69,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://youtu.be/mHHKN72Vx00")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
